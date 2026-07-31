064e61f5b41a5e56d8833701836e4c556eb1f89e
SyntheticBird45 <syntheticbirdxmr@protonmail.com>
2026-07-31 23:21:29 +0000

    feedback edits + convert to tabs as god intended

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cuprate.org',
-  integrations: [mdx(), sitemap()]
+	site: "https://cuprate.org",
+	integrations: [mdx(), sitemap()],
 });
